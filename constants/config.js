37bf23d445356567331bf1b419f67fa3449e2719
Palash-Tinkhede <palash.tinkhede@gmail.com>
2024-10-21 22:39:56 +0530

    Update config.js

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,7 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    "http://localhost:4173",
+    "https://chatbox-frontend-five.vercel.app/login",
     process.env.CLIENT_URL,
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
